dbcff888460edb680dfec41c3d800f4ed7817f9f
Tapan48 <20BDS059@iiitdwd.ac.in>
2025-03-16 04:49:56 +0530

    customized heading in products page

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -24,7 +24,7 @@ const Cards = () => {
 
   return (
     <div className="container mt-3">
-      <h2 className="text-center">Add to Cart Projects</h2>
+      <h2 className="text-center font-bold ">Products</h2>
 
       <div className="row d-flex justify-content-center align-items-center">
         {data.map((element, id) => {
